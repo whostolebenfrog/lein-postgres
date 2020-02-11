c32db9f4c5b5d86841d5cffe6f59db16c7e24ebc
Ben Griffiths <ben@atomist.com>
2020-02-11 15:06:40 +0000

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-postgres "0.1.6-SNAPSHOT"
+(defproject lein-postgres "0.1.6"
   :description "Lein plugin wrapping the open table java embedded postgres component"
   :url "http://github.com/whostolebenfrog/lein-postgres"
   :license {:name "Eclipse Public License"
